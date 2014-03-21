bb6f470a46a3962bff48d65f2f01dbd66f57114b
jmknoll <j.knoll621@gmail.com>
2014-03-21 18:58:06 +0800

    nearly finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+	@user = User.find(params[:id])
   end
+  
+  def new
+	@user = User.new
+  end
+  
+  def create
+	@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "Thank you for signing up! Please check your email to confirm your account."
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
 end
